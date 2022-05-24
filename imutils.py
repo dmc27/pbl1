2e24cf476f1b321fe38d9d65721bed9fd189e0ca
dmc27 <38351366+dmc27@users.noreply.github.com>
2022-05-24 22:19:01 +0900

    Add files via upload

---
+++ imutils.py
@@ -0,0 +1,23 @@
+
+#-------------------------------------------------------------------
+import matplotlib
+import matplotlib.pyplot as plt
+#-------------------------------------------------------------------
+
+def imshow(img, cmap="gray", vmin=0, vmax=1, frameon=False, zoom=1.0):
+
+  dpi = float(matplotlib.rcParams['figure.dpi'])/zoom
+
+  fig = plt.figure(figsize=[img.shape[1]/dpi, img.shape[0]/dpi],
+                   frameon=frameon)
+  ax = fig.add_axes([0, 0, 1, 1])
+  ax.axis('off')
+  ax.imshow(img, cmap=cmap, vmin=vmin, vmax=vmax)
+
+  plt.show()
+#-------------------------------------------------------------------
+
+def show(close=None, block=None):
+
+  plt.show(close, block)
+#-------------------------------------------------------------------
